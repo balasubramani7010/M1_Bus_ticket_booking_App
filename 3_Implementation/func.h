77d3dcf5be53156a2f0ed20bc95c764ae23c63ce
balasubramani7010 <balasrit07@gmail.com>
2022-02-12 11:47:29 +0530

    comm

---
+++ 3_Implementation/func.h
@@ -0,0 +1,9 @@
+#ifndef FUNC_H_INCLUDED
+#define FUNC_H_INCLUDED
+#include "assert.h"
+void booking();
+void availability();
+void cancel();
+void speed();
+void menu();
+#endif
--- 3_Implementation/program.c
+++ 3_Implementation/program.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include"func.c"
-#include"unity.h"
+
 
 int main()
 {
